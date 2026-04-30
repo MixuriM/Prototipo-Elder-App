d4d5479920362fe894a238ba1b9f7c8a2defceba
Marcos Castelli <105806291+MixuriM@users.noreply.github.com>
2026-04-30 19:43:52 -0300

    Fix duplicate base path in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Prototipo-Elder-App/'
   plugins: [react()],
   base: '/Prototipo-Elder-App/'
 })
